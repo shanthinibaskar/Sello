9684bbc0debd8c5af5257f787cecbc049671554a
DNGriffin <dgriffin@wustl.edu>
2018-11-15 17:26:16 -0600

    Firestore
    Added firestore and have it pulling data from the listings collection and printing it.

--- Sello/Sello/ViewController.swift
+++ Sello/Sello/ViewController.swift
@@ -7,13 +7,36 @@
 //
 
 import UIKit
-
+import Firebase
 
 class ViewController: UIViewController {
+    var listings: [Listing] = []
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let db = Firestore.firestore()
+
+        let decoder = JSONDecoder()
+
+        db.collection("listings").whereField("url", isEqualTo: "www.fake.com")
+            .getDocuments() { (querySnapshot, err) in
+                if let err = err {
+                    print("Error getting documents: \(err)")
+                } else {
+                    for document in querySnapshot!.documents {
+                        print("made it here")
+                        print("\(document.documentID) => \(document.data())")
+                        if let data = try?  JSONSerialization.data(withJSONObject: document.data(), options: []) {
+                            let listing = try? decoder.decode(Listing.self, from: data)
+                            self.listings.append(listing!);
+                            print(self.listings);
+                        }
+                        
+                    }
+                }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
